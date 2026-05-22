43bc73822adfabb3639b080b325697b475c857be
ofmarin <ofelmaran@gmail.com>
2026-05-21 21:07:06 -0400

    added dinamic logic to populate the cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject, input, InputSignal } from '@angular/core';
+import { RouterOutlet,  } from '@angular/router';
+import {Card} from './card/card'
+import {CardContent} from './card-content'
+import { Rol } from './rol';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Card],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('four-card-feature-section');
+  cardContent: CardContent = inject(CardContent);
+  roles : Rol[] = this.cardContent.getData();
+  content : InputSignal<Rol[]> = input(this.roles);
+
 }
