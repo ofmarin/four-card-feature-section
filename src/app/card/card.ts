76cca5a2e0d4d344a3fcc93c2c7e4117a7b81569
ofmarin <ofelmaran@gmail.com>
2026-05-26 18:05:31 -0400

    finish mobile and desktop me things, gg todo: deploy and readme

--- src/app/card/card.ts
+++ src/app/card/card.ts
@@ -10,11 +10,14 @@ import { Rol } from '../rol';
 export class Card {
   card = input.required<Rol>();
 
-  borderColor(){
-
-    if(this.card().title === "Supervisor"){
-      return "cyan-border";
+  borderColor() {
+    if (this.card().title === 'Supervisor') {
+      return 'cyan-border';
+    } else if (this.card().title === 'Team Builder') {
+      return 'red-border';
+    } else if (this.card().title === 'Karma') {
+      return 'orange-border';
     }
-    return;
+    return 'blue-border';
   }
 }
